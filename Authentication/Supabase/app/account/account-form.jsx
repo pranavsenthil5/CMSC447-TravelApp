59e45111baeb92f403c03d0982c038b371341a46
Syed Husain <144152175+SyedAlamdarHusain@users.noreply.github.com>
2023-11-03 13:25:57 -0400

    Updated account-form.jsx

--- Authentication/Supabase/app/account/account-form.jsx
+++ Authentication/Supabase/app/account/account-form.jsx
@@ -1,183 +1,151 @@
-'use client'
-import { useCallback, useEffect, useState } from 'react';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import axios from 'axios';
+import styles from './Frame6UserLoginEditUser.module.css';
+import backgroundImage from './travel.png';
+import ellipseImage from './ellipse-3@2x.png';
+import Avatar from './avatar'; // Import the Avatar component
 
 export default function AccountForm({ session }) {
-  const supabase = createClientComponentClient();
   const [loading, setLoading] = useState(true);
-  const [fullname, setFullname] = useState(null);
-  const [username, setUsername] = useState(null);
-  const [website, setWebsite] = useState(null);
-  const [avatar_url, setAvatarUrl] = useState(null);
-  const user = session?.user;
-  
-  const [userData, setUserData] = useState({
-    user_name: "",
-    first_name: "",
-    last_name: "",
-    password_hash: "",
-    location: "",
-    email: session?.user.email, // Include email from Supabase
-
-  });
-  const [accountMessage, setAccountMessage] = useState(""); // State for the account creation message
-
+  const [accountMessage, setAccountMessage] = useState('');
   const apiUrl = 'http://127.0.0.1:5000/account';
+
+  const [userData, setUserData] = useState({
+    user_name: '',
+    first_name: '',
+    last_name: '',
+    password_hash: '',
+    location: '',
+    avatar_url: '', // Add avatar_url to the userData state
+  });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setUserData({ ...userData, [name]: value });
   };
 
-  const createAccount = async () => {
+  const updateProfile = async () => {
     try {
       const response = await axios.post(apiUrl, userData);
 
       if (response.status === 201) {
-        setAccountMessage("Account created successfully!"); // Set the account creation message
+        setAccountMessage('Account updated successfully!');
       } else {
         setAccountMessage(`Error: ${response.data.error}`);
       }
     } catch (error) {
       setAccountMessage(`Error: ${error}`);
     }
-  }
-
-  const getProfile = useCallback(async () => {
-    try {
-      setLoading(true);
-
-      const { data, error, status } = await supabase
-        .from('profiles')
-        .select(`full_name, username, website, avatar_url`)
-        .eq('id', user?.id)
-        .single();
-
-      if (error && status !== 406) {
-        throw error;
-      }
-
-      if (data) {
-        setFullname(data.full_name);
-        setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
-      }
-    } catch (error) {
-      alert('Error loading user data!');
-    } finally {
-      setLoading(false);
-    }
-  }, [user, supabase]);
+  };
 
   useEffect(() => {
-    getProfile();
-  }, [user, getProfile]);
+    // Your getProfile and updateProfile functions remain the same
+  }, [session]);
 
-  async function updateProfile({ username, website, avatar_url }) {
-    try {
-      setLoading(true);
+  const handleSignOut = () => {
+    window.location.href = 'http://localhost:3000';
+  };
 
-      const { error } = await supabase.from('profiles').upsert({
-        id: user?.id,
-        full_name: fullname,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date().toISOString(),
-      });
-      if (error) throw error;
-      alert('Profile updated!');
-    } catch (error) {
-      alert('Error updating the data!');
-    } finally {
-      setLoading(false);
-    }
-  }
+  const handleAvatarUpload = (url) => {
+    setUserData({ ...userData, avatar_url: url });
+  };
 
   return (
-    <div>
-      <h1>User Login</h1>
-      <div className="form-widget">
-        <div>
-          <label htmlFor="email">Email : </label>
-          <input
-            id="email"
-            type="text"
-            value={session?.user.email}
-            style={{ display: 'inline-block', width: '50%' }}
-            disabled
-          />
-        </div>
+    <div className={styles.frame6UserLoginEditUser} style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <img
+        className={styles.frame6UserLogineditUserChild}
+        src={ellipseImage}
+        alt="Ellipse"
+      />
+      <div className={styles.drift}>Drift</div>
 
+      {/* Use a label to style the "Upload Picture" button and associate it with the file input */}
+      <label htmlFor="avatarInput" className={styles.uploadPicture}>
 
-      {/* Your Account Creation Form */}
-      <form>
-        <div className="mb-3">
-          <label>User Name: </label>
-          <input
-            type="text"
-            className="form-control"
-            name="user_name"
-            value={userData.user_name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>First Name: </label>
-          <input
-            type="text"
-            className="form-control"
-            name="first_name"
-            value={userData.first_name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last Name: </label>
-          <input
-            type="text"
-            className="form-control"
-            name="last_name"
-            value={userData.last_name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password: </label>
-          <input
-            type="password"
-            className="form-control"
-            name="password_hash"
-            value={userData.password_hash}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Location: </label>
-          <input
-            type="text"
-            className="form-control"
-            name="location"
-            value={userData.location}
-            onChange={handleChange}
-          />
-        </div>
-        {accountMessage && <p>{accountMessage}</p>} {/* Display the account creation message */}
-        <button type="button" onClick={createAccount} className="btn btn-primary">
-          Create Account
+        <input
+          type="file"
+          id="avatarInput"
+          accept="image/*"
+          onChange={(e) => handleAvatarUpload(e.target.value)}
+          style={{ display: 'none' }}
+        />
+      </label>
+
+      {/* Add the Avatar component here */}
+      <Avatar
+        uid={userData.user_name} // Use a unique identifier, e.g., user_name
+        url={userData.avatar_url}
+        size={150}
+        onUpload={handleAvatarUpload}
+      />
+
+      <div className={styles.editProfile}>Edit Profile</div>
+      <button className={styles.frame6UserLogineditUserItem} onClick={updateProfile}>
+        Update
+      </button>
+      <div className={styles.signOutBox}>
+        <button className={styles.frame6UserLogineditUserInner}>
+          <div className={styles.update}></div>
+          <div>
+            <form action="/auth/signout" method="post">
+              <button className={styles.signOutButton}>Sign out</button>
+            </form>
+          </div>
         </button>
-      </form>
-      <div>
-          <form action="/auth/signout" method="post">
-            <button className="button block" type="submit">
-              Sign out
-            </button>
-          </form>
-        </div>
       </div>
-    </div>
-  );
+      <div className={styles.rectangleDiv} />
+      <div className={styles.userNameFirstContainer}>
+        <span className={styles.userNameFirstContainer1}>
+          <p className={styles.userName}>User Name:</p>
+          <p className={styles.userName}>First Name:</p>
+          <p className={styles.userName}>Last Name:</p>
+          <p className={styles.userName}>Password:</p>
+          <p className={styles.userName}>Location:</p>
+        </span>
+      </div>
+      <Form className={styles.inputBg4}>
+        <Form.Control
+          type="text"
+          name="user_name"
+          value={userData.user_name}
+          onChange={handleChange}
+        />
+      </Form>
+      <Form className={styles.inputBg3}>
+        <Form.Control
+          type="text"
+          name="first_name"
+          value={userData.first_name}
+          onChange={handleChange}
+        />
+      </Form>
+      <Form className={styles.inputBg}>
+        <Form.Control
+          type="text"
+          name="last_name"
+          value={userData.last_name}
+          onChange={handleChange}
+        />
+      </Form>
+      <Form className={styles.inputBg1}>
+        <Form.Control
+          type="password"
+          name="password_hash"
+          value={userData.password_hash}
+          onChange={handleChange}
+        />
+      </Form>
+      <Form className={styles.inputBg2}>
+        <Form.Control
+          type="text"
+          name="location"
+          value={userData.location}
+          onChange={handleChange}
+        />
+      </Form>
+      {accountMessage && <p>{accountMessage}</p>}
+      </div>
+);
 }
 
